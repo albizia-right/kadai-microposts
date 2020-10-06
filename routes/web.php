dc79e666cfc2af50c27e82720f4dc72d64b56669
kouichi.shimizu <programming.gangi@gmail.com>
2020-10-06 06:48:00 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('sighup', 'Auth\RegisterController@register')->name('signup.post');
